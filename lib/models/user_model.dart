b61f697eed02cfefa6dfcd1654b6caa7e19717aa
Hanifuniverse <2206094@itg.ac.id>
2025-06-28 20:48:49 +0700

    menambahkan model artikel dan user

---
+++ lib/models/user_model.dart
@@ -0,0 +1,35 @@
+class User {
+  final String id;
+  final String email;
+  final String name;
+  final String title;
+  final String avatar;
+
+  User({
+    required this.id,
+    required this.email,
+    required this.name,
+    required this.title,
+    required this.avatar,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      email: json['email'],
+      name: json['name'],
+      title: json['title'],
+      avatar: json['avatar'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'email': email,
+      'name': name,
+      'title': title,
+      'avatar': avatar,
+    };
+  }
+}
